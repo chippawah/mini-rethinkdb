ad6ebb71eba3ea3af271af1c18d654bc748e30f9
Chip <charles.cantrell@me.com>
2015-12-08 02:35:59 -0600

    All CRUD methids finished

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var rethinkCtrl = require('./rethinkCtrl')
+
+var app = express()
+app.use(bodyParser.json());
+var port = 9090
+app.listen(port, function(err){
+  if(err){
+    console.log(err);
+  }
+  else {
+    console.log('Server listening on ' + 9090) 
+  }
+});
+
+app.post('/todo', rethinkCtrl.addTodo);
+app.patch('/todo/:id', rethinkCtrl.editTodo);
+app.get('/todo', rethinkCtrl.getTodos);
+app.get('/todo/:id', rethinkCtrl.getTodo);
+app.delete('/todo/:id', rethinkCtrl.removeTodo);
